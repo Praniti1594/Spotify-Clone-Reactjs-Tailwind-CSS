ee487a849dbcaf803e10880da0991d9f888d21e7
Praniti  Kubal <140830559+Praniti1594@users.noreply.github.com>
2024-06-15 08:44:28 -0700

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,198 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+
+import imag1 from './imag1.jpg'
+import imag2 from './imag2.jpg'
+import imag3 from './imag3.jpg'
+import imag4 from './imag4.jpg'
+import imag6 from './imag6.jpg'
+import imag7 from './imag7.jpg'
+import imag8 from './imag8.jpg'
+import imag9 from './imag9.jpg'
+import imag10 from './imag10.jpg'
+import imag11 from './imag11.jpg'
+import imag12 from './imag12.jpg'
+import imag13 from './imag13.jpg'
+import imag14 from './imag14.jpg'
+import imag15 from './imag15.jpg'
+import imag16 from './imag16.jpg'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Summer '24",
+        image: imag13,
+        desc:"Songs which feels like sunshine and daisies",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "let's make the partyy",
+        image: imag9,
+        desc:"Songs which makes you groove",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Main Character",
+        image: imag10,
+        desc:"Songs which makes you feel like a main character",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Shawn Mendes",
+        image: imag4,
+        desc:"Best of Shawn Mendes",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Mega Hits,",
+        image: imag11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: imag7,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Song One",
+        image: imag12,
+        file:song1,
+        desc:"Feel the Vibes",
+        duration:"3:00"
+    },
+    {
+        id:1,
+        name: "Song Two",
+        image: imag2,
+        file:song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:20"
+    },
+    {
+        id:2,
+        name: "Song Three",
+        image: imag1,
+        file:song3,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:32"
+    },
+    {
+        id:3,
+        name: "Song Four",
+        image: imag14,
+        file:song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Song Five",
+        image: imag15,
+        file:song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:10"
+    },
+    {
+        id:5,
+        name: "Song Six",
+        image: imag6    ,
+        file:song3,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:45"
+    },
+    {
+        id:6,
+        name: "Song Seven",
+        image: imag16,
+        file:song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:18"
+    },
+    {
+        id:7,
+        name: "Song Eight",
+        image: imag12,
+        file:song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+]
